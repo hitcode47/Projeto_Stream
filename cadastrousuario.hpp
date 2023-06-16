97be973ab518947c771c7b10ca81f89781b8b5af
DantomGuedes <130753998+DantomGuedes@users.noreply.github.com>
2023-06-16 08:01:13 -0300

    Create cadastrousuario.hpp

---
+++ cadastrousuario.hpp
@@ -0,0 +1,32 @@
+#ifndef "CADASTROUSUARIO_HPP"
+#define "CADASTROUSUARIO_HPP"
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+ 
+class LoginManager {
+    
+public:
+    void login();
+};
+
+
+
+class SignupManager {
+public:
+    void sign_up();
+};
+
+
+
+class Iniciar {
+public:
+    void exibirlogo();
+    void menu();
+    void run();
+};
+
+#endif
