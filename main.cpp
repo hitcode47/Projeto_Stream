b5054cc22468cd1ab4599402a93ed8fd8dccbb60
DantomGuedes <dantomdg@gmail.com>
2023-07-01 16:26:08 -0300

    main atualizada

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
-#include "reproducao.hpp"
-#include "musicas.hpp"
+#include "pesquisa.hpp"
+#include "cadastrousuario.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <limits>
+#include <algorithm>
+#include <vector>
+#include <functional>
+
+std::string nomedeacesso;
+
 
 int main(){
-
-    musicas music;
     
-    music.adicionar_musica("musicas_teste.txt");
-    music.Remove_musica("musicas_teste.txt");
-
+    Iniciar app;
+    
+    app.exibirlogo();
+    nomedeacesso = app.menu();
+    
+    std::cout<<"BEM VINDO "<<nomedeacesso<<std::endl;
+    std::cout<<std::endl;
+    
+    std::string palavra;
+    std::cout << "Digite sua musica/Artista: " << std::endl;
+    std::getline(std::cin, palavra);
+    Pesquisa::Pesquisar Busca(palavra);
+    Busca.Encontrar();
+    
     return 0;
 }
