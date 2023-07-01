90f7fa7a81c0ab0dd48d11fc728f3e7e23ad4029
DantomGuedes <dantomdg@gmail.com>
2023-07-01 16:27:57 -0300

    nova main

--- main.cpp
+++ main.cpp
@@ -8,11 +8,12 @@
 #include <vector>
 #include <functional>
 
-std::string nomedeacesso;
+
 
 
 int main(){
     
+    std::string nomedeacesso;
     Iniciar app;
     
     app.exibirlogo();
@@ -20,7 +21,7 @@ int main(){
     
     std::cout<<"BEM VINDO "<<nomedeacesso<<std::endl;
     std::cout<<std::endl;
-    
+
     std::string palavra;
     std::cout << "Digite sua musica/Artista: " << std::endl;
     std::getline(std::cin, palavra);
